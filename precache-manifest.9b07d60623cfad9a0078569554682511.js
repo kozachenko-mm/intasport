de57d69ea61270240ab479a54ae759ee397f4d30
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2020-06-21 22:48:23 +0300

    Updates

---
+++ precache-manifest.9b07d60623cfad9a0078569554682511.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc00dcf01983dfe053f0ef7ebb2a49c3",
+    "url": "/intasport/index.html"
+  },
+  {
+    "revision": "9fc720d58f8ded372961",
+    "url": "/intasport/static/css/main.d6e13536.chunk.css"
+  },
+  {
+    "revision": "18d531be39821b5a439e",
+    "url": "/intasport/static/js/2.e792bd6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/intasport/static/js/2.e792bd6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc720d58f8ded372961",
+    "url": "/intasport/static/js/main.35ed93cd.chunk.js"
+  },
+  {
+    "revision": "47dfbe6904f95f702897",
+    "url": "/intasport/static/js/runtime-main.7d507f52.js"
+  },
+  {
+    "revision": "539dd6e1ff12fdcb7333f99afa43df57",
+    "url": "/intasport/static/media/emblemsynchronizing_93485.539dd6e1.svg"
+  }
+]);
